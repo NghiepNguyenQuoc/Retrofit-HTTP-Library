286c24c6ec7978aeee5b549785d3af1b44af4dcd
Nghiep Nguyen <nghiep.nguyen@inspireventures.com>
2017-08-17 15:31:52 +0700

    The endpoints are defined inside of an interface using special retrofit annotations to encode details about the parameters and request method. In addition, the return value is always a parameterized Call<T> object such as Call<MovieResponse>. For instance, the interface defines each endpoint in the following way.

--- app/src/main/java/com/nghiepnguyen/retrofithttplibrary/rest/ApiInterface.java
+++ app/src/main/java/com/nghiepnguyen/retrofithttplibrary/rest/ApiInterface.java
@@ -1,8 +1,20 @@
 package com.nghiepnguyen.retrofithttplibrary.rest;
 
+import com.nghiepnguyen.retrofithttplibrary.model.MovieResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
 /**
  * Created by W10-PRO on 17-Aug-17.
  */
 
-public class ApiInterface {
+public interface ApiInterface {
+    @GET("movie/top_rated")
+    Call<MovieResponse> getTopRatedMovies(@Query("api_key") String apiKey);
+
+    @GET("movie/{id}")
+    Call<MovieResponse> getMovieDetails(@Path("id") int id, @Query("api_key") String apiKey);
 }
