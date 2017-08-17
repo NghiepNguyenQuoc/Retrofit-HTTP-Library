7b7ffd0fa371ae78c03ea525b0b2ba5d1a8bee3c
Nghiep Nguyen <nghiep.nguyen@inspireventures.com>
2017-08-17 15:19:59 +0700

    create MainActivity

--- app/src/main/java/com/nghiepnguyen/retrofithttplibrary/activity/MainActivity.java
+++ app/src/main/java/com/nghiepnguyen/retrofithttplibrary/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.nghiepnguyen.retrofithttplibrary;
+package com.nghiepnguyen.retrofithttplibrary.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.nghiepnguyen.retrofithttplibrary.R;
 
 public class MainActivity extends AppCompatActivity {
 
